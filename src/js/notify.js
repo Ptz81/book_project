ebddc202301a145acc81ad9d11197b8835bdf71a
dusixx <113414935+dusixx@users.noreply.github.com>
2023-04-12 14:28:56 +0300

    small fix

--- src/js/notify.js
+++ src/js/notify.js
@@ -1,24 +1,23 @@
 import { Notify } from 'notiflix';
 
-const defNotifyOpts = {
-  timeout: 2500,
-};
-
-// глобальные опции Notify
-Notify.init({ position: 'right-bottom' });
+// опции Notify
+Notify.init({
+  position: 'right-top',
+  timeout: 250,
+});
 
 export function showError(msg, opts) {
-  return Notify.failure(msg, { ...defNotifyOpts, ...opts });
+  return Notify.failure(msg, opts);
 }
 
 export function showInfo(msg, opts) {
-  return Notify.info(msg, { ...defNotifyOpts, ...opts });
+  return Notify.info(msg, opts);
 }
 
 export function showSuccess(msg, opts) {
-  return Notify.success(msg, { ...defNotifyOpts, ...opts });
+  return Notify.success(msg, opts);
 }
 
 export function showWarn(msg, opts) {
-  return Notify.warning(msg, { ...defNotifyOpts, ...opts });
+  return Notify.warning(msg, opts);
 }
