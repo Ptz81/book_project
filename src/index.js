55f97b79446efd74b56979c875fcea1bdd54d32c
Taras Papka <92438445+Ptz81@users.noreply.github.com>
2023-04-14 23:54:03 +0300

    corrected import auth form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './js/toggle-switch';
 import './js/mobile-menu';
 import './js/best-sellers';
 import './js/support-slider';
-import './js/auth/auth-form-login';
+import './js/auth/auth-form-base';
 import './js/pop-up';
 import './js/scroll-up';
