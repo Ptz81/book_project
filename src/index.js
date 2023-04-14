3edad7f8d34e1ea1b542b4014ad07c731959a982
dusixx <113414935+dusixx@users.noreply.github.com>
2023-04-14 17:48:02 +0300

    moves loader to folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import _ from './js/loader';
-import _ from './js/categories';
+import './js/categories';
 import './js/toggle-switch';
-import './js/loader';
 import './js/best-sellers';
 import './js/support-slider';
 import './js/auth/auth-form-base';
