9df11b5486617fc4a63040873baacab072bb4acf
dusixx <113414935+dusixx@users.noreply.github.com>
2023-04-14 09:19:48 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './js/toggle-switch';
 import './js/loader';
 import './js/best-sellers';
-import './js/shopping-list';
-import _ from './js/loader';
-import _ from './js/best-sellers';
 import './js/support-slider';
+import './js/auth/auth-form-base';
