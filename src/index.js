275d7a86ae6de83ae4e2cefe7774b8a852e44fde
dusixx <113414935+dusixx@users.noreply.github.com>
2023-04-16 17:05:58 +0300

    small fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import './js/toggle-switch';
 import './js/mobile-menu';
 import './js/best-sellers';
 import './js/support-slider';
-import './js/auth/auth-form-base';
 import './js/pop-up';
 import './js/scroll-up';
