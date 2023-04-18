a777efe6731e5fb5bb66a30747b11b5192c46606
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-04-18 22:12:32 +0300

    script

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -27,5 +27,39 @@
     });
   })();
 
+  
+// import bodyScrollLock from 'body-scroll-lock';
+
+//   (() => {
+//     const mobileMenu = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
+//     const header = document.querySelector('.page-header');
+//     const headerHeight = header.offsetHeight;
+  
+//     const toggleMenu = () => {
+//       const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//       mobileMenu.classList.toggle('is-open');
+//       if (isMenuOpen) {
+//         document.body.style.paddingTop = 0;
+//       } else {
+//         document.body.style.paddingTop = `${headerHeight}px`;
+//       }
+//       const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+//       bodyScrollLock[scrollLockMethod]();
+//     };
+  
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
+  
+//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//       if (!e.matches) return;
+//       mobileMenu.classList.remove('is-open');
+//       openMenuBtn.setAttribute('aria-expanded', false);
+//       bodyScrollLock.enableBodyScroll(document.body);
+//       document.body.style.paddingTop = 0;
+//     });
+//   })();
 
 
