8dc291ff56210f7009766848b576378eb655ea58
Djoult <55453354+Djoult@users.noreply.github.com>
2023-04-19 19:47:27 +0300

    make a comment

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -6,5 +6,4 @@ import './js/scroll-up';
 import './js/support-render-markup';
 import './js/support-slider';
 import './js/auth/auth-logic';
-import './js/shopping-list/pagination-shopping-list';
-
+// import './js/shopping-list/pagination-shopping-list';
